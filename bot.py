eb73ea0d738f12c0d08e4e77641f5408311674b5
0xyis <82546975+0xyzabc@users.noreply.github.com>
2025-03-16 11:16:38 +0700

    Create bot.py
    code dasar bot discord

---
+++ bot.py
@@ -0,0 +1,33 @@
+import time
+import requests
+
+headers = {
+}
+
+def sendMessage(text):
+    data = {"mobile_network_type":"unknown","content":text,"nonce":str(int(time.time())),"tts":False,"flags":0}
+    headers['Content-Length'] = str(len(str(data)))
+    urlMessage = "Isi" //link discord beserta channel ID
+    res = requests.post(urlMessage,json=data,headers=headers)
+    if res.text.find("id") >= 0:
+        print("[+] pesan berhasil dikirim",text)
+    else:
+        print("[x] pesan gagal dikirim",text)
+
+def countdown(detik):
+    while detik:
+        print(f"[!] tunggu sampai {detik} detik ",flush=True,end="\r")
+        detik -= 1
+        time.sleep(1)
+    print("                              ",flush=True,end="\r")
+
+def main():
+    msg = "hallo semua selamat malam"
+    while True:
+        sendMessage(msg)
+        countdown(60 + 60 ) # 3600
+
+try:
+    main()
+except KeyboardInterrupt:
+    exit()
